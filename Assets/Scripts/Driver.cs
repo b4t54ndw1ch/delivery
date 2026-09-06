39c4c4980d95be01e199f6081a39b885c1e040e9
liacdev <jagoblacksky@gmail.com>
2026-09-06 09:37:42 -0500

    Merge conflicts

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -15,9 +15,13 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0,0,-steerAmount);
         transform.Translate(0,moveAmount,0);
+=======
+        transform.Rotate(0,0,1);
+>>>>>>> 6f3f57d (Test commit for new branch.)
     }
 }
