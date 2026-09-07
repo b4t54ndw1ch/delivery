4ee1757b3b43692b9caf04897002ffc14c6f0ad5
liacdev <jagoblacksky@gmail.com>
2026-09-07 18:41:03 -0500

    2.14 Add FollowCamera script to enable camera tracking of a specified GameObject

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+   [SerializeField] GameObject thingToFollow;
+   // Camera position should be the same as the thingToFollow
+
+    void LateUpdate()
+    {
+         transform.position = thingToFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}
