cb551a10319aca0620c2751ad5b0fc437ca4644e
liacdev <jagoblacksky@gmail.com>
2026-09-08 13:29:12 -0500

    Enhance Delivery script with package handling and customer delivery logic
    - Added package pickup functionality for Triangle, Circle, and Square packages.
    - Implemented delivery logic for Triangle, Circle, and Square customers.
    - Introduced a delay for package destruction upon pickup.
    - Updated TagManager to include new tags: House, Rock, and Tree.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,37 +2,114 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-   void OnCollisionEnter2D(Collision2D other)
-   {
-      Debug.Log("Ouch! Collision happened.");
-   }
+    [SerializeField] float destroyDelay = 0.1f;
+    bool hasTriangle = false;
+    bool hasCircle = false;
+    bool hasSquare = false;
+    bool hasPackage = false;
+
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Ouch! Collision happened.");
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "TrianglePackage")
+        PackagePickup();
+        PackageDelivery();
+
+
+        void PackagePickup()
         {
-            Debug.Log("TrianglePackage received.");
-        }else if (other.tag == "CirclePackage")
-        {
-            Debug.Log("CirclePackage received.");
-        }
-        else if (other.tag == "SquarePackage")
-        {
-            Debug.Log("SquarePackage received.");
-        } 
-        
-        if (other.tag == "TriangleCustomer")
-        {
-            Debug.Log("TriangleCustomer delivered.");
-        }else if (other.tag == "CircleCustomer")
-        {
-            Debug.Log("CircleCustomer delivered.");
-        }
-        else if (other.tag == "SquareCustomer")
-        {
-            Debug.Log("SquareCustomer delivered.");
+            if (other.tag == "TrianglePackage")
+            {
+                if (hasPackage)
+                {
+                    Debug.Log("You already have a Package.");
+                    return;
+                }
+                else
+                {
+                    Destroy(other.gameObject, destroyDelay);
+                    hasTriangle = true;
+                    hasPackage = true;
+                    Debug.Log("TrianglePackage received.");
+                }
+            }
+
+            if (other.tag == "CirclePackage")
+            {
+                if (hasPackage)
+                {
+                    Debug.Log("You already have a Package.");
+                    return;
+                }
+                else
+                {
+                    Destroy(other.gameObject, destroyDelay);
+                    hasCircle = true;
+                    hasPackage = true;
+                    Debug.Log("CirclePackage received.");
+                }
+            }
+
+            if (other.tag == "SquarePackage")
+            {
+                if (hasPackage)
+                {
+                    Debug.Log("You already have a Package.");
+                    return;
+                }
+                else
+                {
+                    Destroy(other.gameObject, destroyDelay);
+                    hasSquare = true;
+                    hasPackage = true;
+                    Debug.Log("SquarePackage received.");
+                }
+            }
         }
 
+        void PackageDelivery()
+        {
+            if (other.tag == "TriangleCustomer" && hasTriangle)
+            {
+                hasTriangle = false;
+                hasPackage = false;
+                Debug.Log("TriangleCustomer delivered.");
+            }
+            else
+            {
+                Debug.Log("TriangleCustomer detected.");
+            }
+
+            if (other.tag == "CircleCustomer" && hasCircle)
+            {
+                hasCircle = false;
+                hasPackage = false;
+                Debug.Log("CircleCustomer delivered.");
+            }
+            else
+            {
+                Debug.Log("CircleCustomer detected.");
+            }
+
+            if (other.tag == "SquareCustomer" && hasSquare)
+            {
+                hasSquare = false;
+                hasPackage = false;
+                Debug.Log("SquareCustomer delivered.");
+            }
+            else
+            {
+                Debug.Log("SquareCustomer detected.");
+            }
+        }
     }
-
 }
+
+
+
+
